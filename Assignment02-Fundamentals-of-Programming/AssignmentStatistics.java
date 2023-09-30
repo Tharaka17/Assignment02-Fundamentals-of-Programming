f571ba0c0881c8fc8cf00cfa6c267eb9db7b50bf
Tharaka17 <tharakaarrow@gmail.com>
2023-09-30 17:57:14 +1000

    processed and stored in Student objects

--- Assignment02-Fundamentals-of-Programming/AssignmentStatistics.java
+++ Assignment02-Fundamentals-of-Programming/AssignmentStatistics.java
@@ -38,9 +38,37 @@ class Student {
 
 public class AssignmentStatistics {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String unitName = "";
         List<Student> students = new ArrayList<>();
-        String filePath = "F:\WALLPAPER\Assignment02\Assignment02-Fundamentals-of-Programming\Assignment02-Fundamentals-of-Programming\student-grade.txt";
+        String filePath = "E:\student-grade.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.trim().isEmpty() || line.trim().startsWith(" ")) {
+                    continue;
+                }
+
+                if (line.startsWith("Unit Name:")) {
+                    unitName = line.substring(11).trim();
+                } else {
+                    String[] parts = line.split("\\s+");
+                    if (parts.length >= 5) {
+                        String lastName = parts[0];
+                        String firstName = parts[1];
+                        String studentID = parts[2];
+                        double assignment1 = Double.parseDouble(parts[3]);
+                        double assignment2 = Double.parseDouble(parts[4]);
+                        double assignment3 = Double.parseDouble(parts[5]);
+
+                        Student student = new Student(lastName, firstName, studentID, assignment1, assignment2, assignment3);
+                        students.add(student);
+                    }
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
